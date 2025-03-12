3a1df174d079bf366264370894076df732860279
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-03-12 03:39:58 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
-// tailwind.config.js
-module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    darkMode: 'class', // Enable dark mode using the 'class' strategy
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  }
+}
